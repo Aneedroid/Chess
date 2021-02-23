5076495a823231b3c7f21cfd8bee698f77ab72ae
Aneedroid <aneedroid13@gmail.com>
2021-02-24 01:14:06 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Board from './Board.svelte';
+	export let name;
+</script>
+
+<main>
+	<h1>Chess</h1>
+	<Board />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		/* padding: 0 0.25rem; */
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 100;
+	}
+
+	/* @media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
---
+++ src/Square.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let colorIndex;
+
+    const squareClass = (colorIndex) => {
+        let color;
+        const row = Math.floor(colorIndex/8);
+
+        if(row % 2 == 0) {
+            // Starts with lite.
+            if((colorIndex % 2) == 0) color = 'lite';
+            else color = 'dark';
+        }
+        else {
+            if((colorIndex % 2) == 0) color = 'dark';
+            else color = 'lite';
+        }
+        return color;
+    };
+</script>
+
+<style>
+    div {
+        height: 4rem;
+        width: 4rem;
+        background-color: hotpink;
+    }
+
+    .lite {
+        background-color: #F5DEB3;
+    }
+
+    .dark {
+        background-color: #8B4513;
+    }
+</style>
+
+<div class={squareClass(colorIndex)}>{colorIndex}</div>
